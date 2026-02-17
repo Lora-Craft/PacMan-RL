edbdecbe822ec57cd700196b82bedc8c06471837
LoraCraft <eamonnjdavis@gmail.com>
2026-02-17 17:14:08 +1100

    Added rudimentary reward tracking and episode step counter. Fixed bug with tensordict not advancing with import of step_mdp.

--- src/env.py
+++ src/env.py
@@ -5,9 +5,11 @@ from torchrl.envs import (
     DoubleToFloat,
     ObservationNorm,
     StepCounter,
+    RewardSum,
 )
 from torchrl.envs.transforms import Resize, Compose, ToTensorImage
 from torchvision.transforms import InterpolationMode
+from torchrl.envs.utils import step_mdp
 import torch as t
 from discretizer import Discretizer
 from model import PMAlpha, PMPolicy, PMValue
@@ -45,7 +47,8 @@ def make_env(
         Resize(84, 84, interpolation=InterpolationMode.NEAREST),
         #ObservationNorm(in_keys=["observation"]),
         #DoubleToFloat(),
-        #StepCounter(),
+        StepCounter(),
+        RewardSum(),
     ]))
 
     return transformed_env
@@ -59,13 +62,14 @@ if __name__ == "__main__":
     value = PMValue(backbone)
     backbone.eval()
     tensordict = test.reset()
+    ep_reward = 0.0
 
     while True:
         obs = tensordict["pixels"]
         
         obs_batch = obs.unsqueeze(0) # adds batch dimensions for model
-        print(obs)
-        print(obs_batch)
+        #print(obs)
+        #print(obs_batch)
         with t.no_grad():
             #logits, value = model(obs_batch)
             logits = policy(obs_batch)
@@ -75,15 +79,25 @@ if __name__ == "__main__":
         action = t.multinomial(a_probs, 1)
 
         tensordict = test.step(tensordict.set("action", get_torch_compatible_actions(action, num_actions=5)))
-        reward = tensordict["next", "reward"]
+        reward = tensordict["next", "reward"].item()
         term = tensordict["next", "terminated"]
         trunc = tensordict["next", "truncated"]
+        ep_reward += reward
+        #print(f"terminated: {term}, truncated: {trunc}")
+    
+        if tensordict["next", "done"].any():
+            ep_length = tensordict["next", "step_count"].item()
 
         if term or trunc:
             print("EPISODE ENDED")
-            print(obs, reward, term, trunc)
-            print(tensordict)
-            test.reset()
+            #print(f"TENSORDICT: {tensordict}")
+            print(ep_reward)
+            print(ep_length)
+            ep_reward = 0.0
+            tensordict = test.reset()
+        
+        else:
+            tensordict = step_mdp(tensordict)
     
 
     
